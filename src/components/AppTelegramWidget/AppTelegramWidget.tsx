e77f55294ae93ccc2ee1d9ed1ef68697c4d2168d
Dmitry Trofimov <dimkaboin@yandex.ru>
2024-01-22 13:37:52 +0300

    fix tariff

--- src/components/AppTelegramWidget/AppTelegramWidget.tsx
+++ src/components/AppTelegramWidget/AppTelegramWidget.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const AppTelegramWidget = () => {
     return (
-        <Link href="https://t.me/marketdbru" className="flex gap-3 bg-white rounded-lg m-6 p-2">
+        <Link href="https://t.me/marketdbchat" target="_blank" className="flex gap-3 bg-white rounded-lg m-6 p-2">
             <div className="w-8 h-8 rounded-full overflow-hidden relative">
                 <Image 
                     src="/chat-avatar.jpg"
@@ -14,7 +14,7 @@ export const AppTelegramWidget = () => {
             </div>
             <div className="flex flex-col gap">
                 <div className="text-base text-black-800 font-semibold">Telegram</div>
-                <div className="text-xs text-gray font-medium text-[#a7a7a7]">1122 подписчиков</div>
+                <div className="text-xs text-gray font-medium text-[#a7a7a7]">1к+ подписчиков</div>
             </div>
         </Link>
     );
--- src/components/ui/popup/data.tsx
+++ src/components/ui/popup/data.tsx
@@ -18,7 +18,7 @@ export const pricing = [
       },
       {
         limit: true,
-        title: "Позиции товара"
+        title: "История позиций товара"
       },
       {
         limit: true,
@@ -26,11 +26,23 @@ export const pricing = [
       },
       {
         limit: true,
-        title: "3 отчета по магазинам в сутки"
+        title: "Отчеты по магазинам в формате Excel"
+      },
+      {
+        limit: true,
+        title: "Общая аналитика по магазинам"
+      },
+      {
+        limit: true,
+        title: "Все магазины продавцов"
+      },
+      {
+        limit: true,
+        title: "Общая аналитика по категорииям"
       },
       {
         limit: false,
-        title: "2 отчета Excel по категориям в сутки"
+        title: "Отчеты по категориям в формате Excel"
       },
       {
         limit: false,
@@ -55,19 +67,31 @@ export const pricing = [
       },
       {
         limit: true,
-        title: "Позиции товара"
+        title: "История позиций товара"
       },
       {
         limit: true,
-        title: "30 / 60 дней периода аналитики"
+        title: "30 / 60 / 90 дней периода аналитики"
       },
       {
         limit: true,
-        title: "6 отчетов Excel по магазинам в сутки"
+        title: "Отчеты по магазинам в формате Excel"
       },
       {
         limit: true,
-        title: "2 отчета Excel по категориям в сутки"
+        title: "Отчеты по категориям в формате Excel"
+      },
+      {
+        limit: true,
+        title: "Общая аналитика по магазинам"
+      },
+      {
+        limit: true,
+        title: "Все магазины продавцов"
+      },
+      {
+        limit: true,
+        title: "Общая аналитика по категорииям"
       },
       {
         limit: true,
@@ -92,7 +116,7 @@ export const pricing = [
       },
       {
         limit: true,
-        title: "Позиции товара"
+        title: "История позиций товара"
       },
       {
         limit: true,
@@ -100,11 +124,23 @@ export const pricing = [
       },
       {
         limit: true,
-        title: "15 отчетов Excel по магазинам в сутки"
+        title: "Отчеты по магазинам в формате Excel"
       },
       {
         limit: true,
-        title: "4 отчета Excel по категориям в сутки"
+        title: "Отчеты по категориям в формате Excel"
+      },
+      {
+        limit: true,
+        title: "Общая аналитика по магазинам"
+      },
+      {
+        limit: true,
+        title: "Все магазины продавцов"
+      },
+      {
+        limit: true,
+        title: "Общая аналитика по категорииям"
       },
       {
         limit: true,
