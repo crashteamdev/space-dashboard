933b38918e777fca2ef891c18d8db4688bd5991c
vitaxa <vitaxa93gamebox@gmail.com>
2024-12-01 17:15:09 +0300

    fix linter error

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,23 +1,24 @@
 "use client";
-import posthog from 'posthog-js'
-import {PostHogProvider} from 'posthog-js/react'
+import React from "react";
+import posthog from "posthog-js";
+import {PostHogProvider} from "posthog-js/react";
 
 interface IPropsProviders {
     children: React.ReactNode;
 }
 
 export const Providers = ({children}: IPropsProviders) => {
-    if (typeof window !== 'undefined') {
-        posthog.init('phc_QhlnZCxS3pf7iwRwlPdlfxvJCurFRoVQPsCHy1mZpBW',
+    if (typeof window !== "undefined") {
+        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "",
             {
-                api_host: 'https://eu.i.posthog.com',
-                person_profiles: 'identified_only'
+                api_host: "https://eu.i.posthog.com",
+                person_profiles: "identified_only"
             }
-        )
+        );
     }
     return (
         <PostHogProvider client={posthog}>
             {children}
         </PostHogProvider>
-    )
-}
+    );
+};
